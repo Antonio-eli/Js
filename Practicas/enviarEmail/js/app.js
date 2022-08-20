cb6ad617446a3503d4bf9ddf7f78fdcf13ecc571
Antonio-eli <eliceoroan@gmail.com>
2022-08-20 14:50:14 -0500

    Add Expresion regular JS

--- Practicas/enviarEmail/js/app.js
+++ Practicas/enviarEmail/js/app.js
@@ -26,17 +26,36 @@ function iniciarApp() {
 
 function validarFormulario(e) {
     if (e.target.value.length > 0) {
-        console.log("si hay algo");
+        //Elimina el mensaje de error.
+        const error = document.querySelector('p.error');
+        error.remove();
+        e.target.classList.remove('border', 'border-red-500');
+        e.target.classList.add('border', 'border-green-500');
     } else {
-        //e.target.style.borderBottomColor = 'red';
+        e.target.classList.remove('border', 'border-green-500');
         e.target.classList.add('border', 'border-red-500');
-        mostrarError();
+        mostrarError('Todos los campos son obligatorios');
+    }
+
+    if (e.target.type === 'email') {
+        //Expresion regular JS
+        const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        if (emailRegex.test(e.target.value)) {
+            const error = document.querySelector('p.error');
+            error.remove();
+            e.target.classList.remove('border', 'border-red-500');
+            e.target.classList.add('border', 'border-green-500');
+        } else {
+            e.target.classList.remove('border', 'border-green-500');
+            e.target.classList.add('border', 'border-red-500');
+            mostrarError('No es un email valido');
+        }
     }
 }
 
-function mostrarError() {
+function mostrarError(mensaje) {
     const mensajeError = document.createElement('p');
-    mensajeError.textContent = 'Todos los campos son obligatorios';
+    mensajeError.textContent = mensaje;
     mensajeError.classList.add('bg-red-100', 'border', 'border-l-4', 'border-red-400', 'text-red-700', 'px-4', 'py-3', 'mt-2', 'rounded', 'text-center', 'error');
     const errores = document.querySelectorAll('.error');
     if (errores.length === 0) {
